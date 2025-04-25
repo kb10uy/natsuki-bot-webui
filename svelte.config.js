bde7f61e140fd0a51a34eddc912486ce7d1c045c
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-04-26 02:41:40 +0900

    Use cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
 };
 
 export default config;
